12e8b89992c4722a16aadf0e375aa0834133a16c
Alejandro <al.sepulvedam@profesor.duoc.cl>
2022-09-30 15:19:26 -0300

    login y servicio

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
+import { ComidasService } from '../../services/comidas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(
+    private st:Storage, 
+    private rt:Router,
+    private srvComida:ComidasService) {}
 
+  ngOnInit() {
+    //console.log(this.srvComida.getListaComidas());
+    this.cargarComidas();
+  }
+
+
+  cargarComidas()
+  {
+    
+    this.srvComida.getListaComidas().subscribe(datos=>
+      {
+        console.log(datos);
+      });
+  }
+
+  cerrarSesion(){
+    this.logout();
+    this.rt.navigate(['/login']);
+  }
+
+  async logout()
+  {
+    await this.st.set('session',null);
+  }
 }
